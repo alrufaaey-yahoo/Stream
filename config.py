69809d46bb6a92957e6383382cf97a3160c8cdfe
Eren Yeager <theerenyeagerx@gmail.com>
2025-12-28 13:24:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ OWNER_ID = int(os.getenv("OWNER_ID", 0))
 API_ID = int(os.getenv("API_ID", 0))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-DEFAULT_RTMP_URL = ("rtmps://dc5-1.rtmp.t.me/s/")
+DEFAULT_RTMP_URL = "rtmps://dc5-1.rtmp.t.me/s/"
 LOGGER_ID = int(os.getenv("LOGGER_ID", 0))
 MONGO_URL = os.getenv("MONGO_URL", "")
