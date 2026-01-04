7087e036a64cff4e2e42580c4c3914e1801e3dcc
alrufaaey-yahoo <alrufaaey@yahoo.com>
2026-01-04 16:00:33 +0200

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ OWNER_ID = int(os.getenv("OWNER_ID", 7851734792))
 API_ID = int(os.getenv("API_ID", 34179581))
 API_HASH = os.getenv("API_HASH", "d1643888aaf2da525b496d9f738f3668")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8373798184:AAHAoDZKvSejuwYYbBlFBtDyiHNZGJJiOTc")
-DEFAULT_RTMP_URL = "rtmps://dc4-1.rtmp.t.me/s/2519683084:WtASbEKfu1AWCSWEo94cgA"
+DEFAULT_RTMP_URL = "rtmps://dc4-1.rtmp.t.me/s/"
 LOGGER_ID = int(os.getenv("LOGGER_ID", -1002519683084))
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://alrufaaey:engmomo@cluster0.codtlum.mongodb.net/?appName=Cluster0")
