dfc99f047d2045e2af187b0abf3812e85090bdcf
alrufaaey-yahoo <alrufaaey@yahoo.com>
2026-01-04 14:49:52 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OWNER_ID = int(os.getenv("OWNER_ID", 0))
-API_ID = int(os.getenv("API_ID", 0))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-DEFAULT_RTMP_URL = "rtmps://dc5-1.rtmp.t.me/s/"
-LOGGER_ID = int(os.getenv("LOGGER_ID", 0))
-MONGO_URL = os.getenv("MONGO_URL", "")
+OWNER_ID = int(os.getenv("OWNER_ID", 7851734792))
+API_ID = int(os.getenv("API_ID", 34179581))
+API_HASH = os.getenv("API_HASH", "d1643888aaf2da525b496d9f738f3668")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8373798184:AAHAoDZKvSejuwYYbBlFBtDyiHNZGJJiOTc")
+DEFAULT_RTMP_URL = "rtmps://dc4-1.rtmp.t.me/s/2519683084:WtASbEKfu1AWCSWEo94cgA"
+LOGGER_ID = int(os.getenv("LOGGER_ID", -1002519683084))
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://alrufaaey:engmomo@cluster0.codtlum.mongodb.net/")
